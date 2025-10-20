d36dc6e434a70314ef688bc27a229e0ebfdd1c4a
carlos <apolayamendozacarlos@gmail.com>
2025-10-20 15:19:23 -0500

    Se modifica la consulta SQL y se pinta en el index.html

--- backend/src/controller/cursosController.js
+++ backend/src/controller/cursosController.js
@@ -1,8 +1,23 @@
 const db = require("../config/db");
 
 exports.obtenerCursos = async (req, res) => {
-  const sql =
-    "SELECT id_curso, titulo, descripcion, precio, duracion_horas, id_subcategoria, id_docente, fecha_inicio, fecha_fin FROM cursos";
+  const sql = `
+    SELECT 
+      c.id_curso,
+      c.titulo,
+      c.descripcion,
+      c.precio,
+      c.duracion_horas,
+      s.nombre AS subcategoria,
+      cat.nombre AS categoria,
+      d.nombre AS docente,
+      c.fecha_inicio,
+      c.fecha_fin
+    FROM cursos c
+    INNER JOIN subcategorias s ON c.id_subcategoria = s.id_subcategoria
+    INNER JOIN categorias cat ON s.id_categoria = cat.id_categoria
+    INNER JOIN docentes d ON c.id_docente = d.id_docente
+  `;
 
   try {
     const [rows] = await db.query(sql);
