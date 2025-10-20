1e5fd0a56437166358292926d4f9a4a8fb965a53
carlos <apolayamendozacarlos@gmail.com>
2025-10-20 15:23:44 -0500

    Se modifica la consulta SQL y se pinta en el index.html

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,14 +1,32 @@
-const getCategorias = async () => {
+document.addEventListener("DOMContentLoaded", () => {
+  getCursos();
+});
+
+const getCursos = async () => {
+  const cursosContainer = document.getElementById("cursos-container");
+
   try {
-    const response = await fetch("http://localhost:3001/api/categorias");
+    const response = await fetch("http://localhost:3001/api/cursos");
     const data = await response.json();
-    console.log(data);
+    
+    data.forEach(curso => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      card.innerHTML = `
+        <div class="card-body">
+          <h5 class="card-title">${curso.titulo}</h5>
+          <p class="card-text">${curso.descripcion}</p>
+          <p class="card-text"><strong>Precio:</strong> ${curso.precio}</p>
+          <p class="card-text"><strong>Duración:</strong> ${curso.duracion_horas} horas</p>
+          <p class="card-text"><strong>Docente:</strong> ${curso.docente}</p>
+        </div>
+      `;
+
+      cursosContainer.appendChild(card);
+    });
+
   } catch (error) {
     console.error(error);
   }
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM fully loaded and parsed");
-  getCategorias();
-});
